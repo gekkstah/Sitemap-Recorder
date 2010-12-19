0825f774a09508732d709558d790b4ed08d4f652
Bjoern K <Test@.(none)>
2010-12-19 22:14:03 +0100

    added z-index to textarea

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -48,6 +48,7 @@ var generator = {
 	ta.style.position = "fixed";
 	ta.style.top = 0;
 	ta.style.left = 0;
+	ta.zIndex = 1010;
 	ta.style.overflow = "visible";
 	ta.disabled = "disabled";
 	window.content.document.getElementsByTagName("body")[0].appendChild(ta);
